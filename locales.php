b2c399fe13ab04a355ed7b7129813a5dc9ec1a49
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-12-22 20:55:26 +0100

    Add locales.php used for listing locales

---
+++ locales.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * FOSSBilling.
+ *
+ * @copyright FOSSBilling (https://www.fossbilling.org)
+ * @license   Apache-2.0
+ *
+ * Copyright FOSSBilling 2022
+ *
+ * This source file is subject to the Apache-2.0 License that is bundled
+ * with this source code in the file LICENSE
+ */
+
+return [
+  
+ 'en_US' => 'English',
+ 'ar_EG' => 'عربى',
+ 'de_DE' => 'Deutsch',
+ 'el_EL' => 'ελληνικά',
+ 'es_ES' => 'Español',
+ 'fr_FR' => 'Français',
+ 'he_IL' => 'Hindi',
+ 'nl_NL' => 'Nederlands',
+ 'pt_PT' => 'Português',
+ 'vi_VI' => 'Tiếng Việt',
+ 'zh_CN' => '大陆简体中文',
+ 'zh_TW' => '臺灣正體中文',
+  ];
